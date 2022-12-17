634d464d7b6d270b90ac2315f3233e6b3f5912c8
Yana-Lesina <yanalesina3@gmail.com>
2022-12-17 22:14:31 +0300

    create register & ligin forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import Home from './Pages/Home';
-import Navbar from './components/Navbar';
-import {Routes, Route} from 'react-router-dom';
-import Paitings from './components/Paintings';
-import Paiting from './components/Paiting';
-import Exhibitions from './components/Exhibitions';
-import Exhibition from './components/Exhibition';
-import Artist from './components/Artist';
-import Artists from './components/Artists';
-import About from './components/About';
-import Tours from './components/Tours';
-import Register from './components/Register';
+import Home from "./Pages/Home";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Paitings from "./components/Paintings";
+import Paiting from "./components/Paiting";
+import Exhibitions from "./components/Exhibitions";
+import Exhibition from "./components/Exhibition";
+import Artist from "./components/Artist";
+import Artists from "./components/Artists";
+import About from "./components/About";
+import Tours from "./components/Tours";
+import Register from "./components/Register";
+import Registration from "./components/Registration";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/paintings' element={<Paitings/>}/>
-        <Route path='/paintings/:id' element={<Paiting/>}/>
-        <Route path='/exhibitions' element={<Exhibitions/>}/>
-        <Route path='/exhibitions/:id' element={<Exhibition/>}/>
-        <Route path='/artists' element={<Artists/>}/>
-        <Route path='/artists/:id' element={<Artist/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/exhibitions/:id/tours' element={<Tours/>}/>
-        <Route path='/register' element={<Register/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/paintings" element={<Paitings />} />
+        <Route path="/paintings/:id" element={<Paiting />} />
+        <Route path="/exhibitions" element={<Exhibitions />} />
+        <Route path="/exhibitions/:id" element={<Exhibition />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/artists/:id" element={<Artist />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/exhibitions/:id/tours" element={<Tours />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
